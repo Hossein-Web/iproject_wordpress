3c13cf5abd696b7861ada917d5b29814c5e27bb2
Hossein-web <mostajeranhossein73@gmail.com>
2020-09-01 14:48:28 +0430

    web_design_sec shortcode was added

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -1,52 +1,106 @@
 <?php
-function wordpressify_resources() {
-	wp_enqueue_style( 'style', get_stylesheet_uri() );
-	wp_enqueue_script( 'header_js', get_template_directory_uri() . '/js/header-bundle.js', null, 1.0, false );
-	wp_enqueue_script( 'footer_js', get_template_directory_uri() . '/js/footer-bundle.js', null, 1.0, true );
+function wordpressify_resources()
+{
+    wp_enqueue_style('style', get_stylesheet_uri());
+    wp_enqueue_script('header_js', get_template_directory_uri() . '/js/header-bundle.js', null, 1.0, false);
+    wp_enqueue_script('footer_js', get_template_directory_uri() . '/js/footer-bundle.js', null, 1.0, true);
 }
 
-add_action( 'wp_enqueue_scripts', 'wordpressify_resources' );
+add_action('wp_enqueue_scripts', 'wordpressify_resources');
 
 // Customize excerpt word count length
-function custom_excerpt_length() {
-	return 22;
+function custom_excerpt_length()
+{
+    return 22;
 }
 
-add_filter( 'excerpt_length', 'custom_excerpt_length' );
+add_filter('excerpt_length', 'custom_excerpt_length');
 
 // Theme setup
-function wordpressify_setup() {
-	// Handle Titles
-	add_theme_support( 'title-tag' );
+function wordpressify_setup()
+{
+    // Handle Titles
+    add_theme_support('title-tag');
 
-	// Add featured image support
-	add_theme_support( 'post-thumbnails' );
-	add_image_size( 'small-thumbnail', 720, 720, true );
-	add_image_size( 'square-thumbnail', 80, 80, true );
-	add_image_size( 'banner-image', 1024, 1024, true );
+    // Add featured image support
+    add_theme_support('post-thumbnails');
+    add_image_size('small-thumbnail', 720, 720, true);
+    add_image_size('square-thumbnail', 80, 80, true);
+    add_image_size('banner-image', 1024, 1024, true);
 }
 
-add_action( 'after_setup_theme', 'wordpressify_setup' );
+add_action('after_setup_theme', 'wordpressify_setup');
 
-show_admin_bar( false );
+show_admin_bar(false);
 
 // Checks if there are any posts in the results
-function is_search_has_results() {
-	return 0 != $GLOBALS['wp_query']->found_posts;
+function is_search_has_results()
+{
+    return 0 != $GLOBALS['wp_query']->found_posts;
 }
 
 // Add Widget Areas
-function wordpressify_widgets() {
-	register_sidebar(
-		array(
-			'name'          => 'Sidebar',
-			'id'            => 'sidebar1',
-			'before_widget' => '<div class="widget-item">',
-			'after_widget'  => '</div>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
+function wordpressify_widgets()
+{
+    register_sidebar(
+        array(
+            'name' => 'Sidebar',
+            'id' => 'sidebar1',
+            'before_widget' => '<div class="widget-item">',
+            'after_widget' => '</div>',
+            'before_title' => '<h2 class="widget-title">',
+            'after_title' => '</h2>',
+        )
+    );
 }
 
-add_action( 'widgets_init', 'wordpressify_widgets' );
+add_action('widgets_init', 'wordpressify_widgets');
+
+// shortcodes
+//web design sec
+function web_design_sec_shortcode($atts)
+{
+    ob_start(); ?>
+    <section class="web_design_sec">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="web_design_sec__text">
+                        <div class="title title--red">
+                            <h4><?php the_field('web_design_sec_title_fa'); ?></h4>
+                            <p><?php the_field('web_design_sec_title_en'); ?></p>
+                        </div><!-- .title -->
+                        <p><?php the_field('web_design_sec_description'); ?></p>
+                    </div><!-- .web_design_sec_text__content -->
+                </div><!-- .col-xl-10 -->
+                <div class="col-xl-12">
+                    <div class="web_design_sec__img_wrapper">
+                        <img src="http://127.0.0.1:3020/wp-content/uploads/2020/09/web_design_sec.png"
+                             alt="section image">
+                    </div><!-- .web_design_sec__img_wrapper -->
+                </div><!-- .col-xl-12 -->
+            </div><!-- .row -->
+        </div><!-- .container-->
+        <a class="web_design_sec__go_bottom" href="#">
+            <span class="icon-arrow-down"></span>
+        </a><!-- .web_design_sec__go_bottom -->
+    </section><!-- .web_design_sec -->
+    <?php
+    return ob_get_clean();
+}
+
+add_shortcode('web_design_sec', 'web_design_sec_shortcode');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
