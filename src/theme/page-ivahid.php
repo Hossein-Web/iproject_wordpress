4dd3069b2c6dffbc33f28187f9da95f36f9421a7
akram-web <email3307@gmail.com>
2020-10-03 15:23:45 +0330

    rating was added

--- src/theme/page-ivahid.php
+++ src/theme/page-ivahid.php
@@ -1,17 +1,30 @@
 <?php /* Template Name: ivahid */ ?>
 <?php get_header(); ?>
+<?php
+if (have_posts()){
+while (have_posts()){
+the_post();
+?>
 <article class="web-design-page">
     <div class="web-design-page__content">
         <div class="container">
-            <?php
-            if ( have_posts() ){
-                while ( have_posts() ){
-                    the_post();
+                 <?php
                     the_content();
-                }
-            }
-            ?>
+                    ?>
         </div><!-- .container -->
     </div>
 </article>
+<div class="rattingpostpage" >
+    امتیاز شما به این صفحه از نظر راهنمایی ؟
+    <?php if(function_exists('the_ratings')) { the_ratings(); schema();} ?>
+</div>
+    <?php
+      }
+    }
+else{
+    echo "چیزی پیدا نشد";
+}
+	wp_reset_query();
+?>
+<?php comments_template(); ?>
 <?php get_footer(); ?>
